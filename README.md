9c8d95969f49cd1d92dadb5d125dff02aa27db7c
Jim Cummins <jim.for.cy@gmail.com>
2014-05-13 10:26:11 -0500

    Add config file functionality for multiple users at once.

--- README.md
+++ README.md
@@ -4,13 +4,45 @@ HipFM
 Post "now playing" track from last.fm to HipChat room.
 
 
-## Usage
+## Command line usage
 
 node server.js [last.fm API Key] [last.fm User] [HipChat Key] [HipChat Room] [HipChat Name]
 
 ```shell
 node server.js THISISMYLASTFMAPIKEY blueflagmusic HIPCHATKEY "My Room" "HipFM"
 ```
+
+## Config file (hipfm.json) usage
+
+```shell
+node server.json -f
+```
+
+** Example **
+
+```json
+{
+    "hipchat": {
+        "key": "11111111111111111111111111111111",
+        "room": "Music"
+    },
+    "lastfm": {
+        "users": [
+            {
+                "displayName": "User1Music",
+                "username": "lastfm_username1",
+                "key": ""
+            },
+            {
+                "displayName": "User2Music",
+                "username": "lastfm_user2",
+                "key": "11111111111111111111111111111111"
+            }
+        ]
+    }
+}
+```
+
 
 * **last.fm API Key**: Get one here [Last.fm Web Services](http://www.last.fm/api/account/create)
 * **last.fm User**: The user's playlist you want to follow (eg. [blueflagmusic](http://ws.audioscrobbler.com/1.0/user/blueflagmusic/recenttracks.rss))
--- server.js
+++ server.js
@@ -1,46 +1,72 @@
-if (process.argv.length < 6) {
-    console.log('Usage: node server.js ' + ' [Last.fm Api Key] [Last.fm User] [HipChat Admin/Notification Key] [Room] [Display Name]');
-    process.exit(1);
-}
-
-var HIPCHAT_AUTH_TOKEN = process.argv[4],
-    HIPCHAT_ROOM = process.argv[5],
-    HIPCHAT_DISPLAY_NAME = process.argv[6] || 'HipFM',
-    LASTFM_API = process.argv[2],
-    LASTFM_USER = process.argv[3];
-
 var _ = require('lodash'),
     util = require('util'),
     http = require('http'),
-    https = require('https');
+    https = require('https'),
+    fs = require('fs');
 
-var lastTrack = '';
+var config = {};
 
-function checkTracks() {
-    var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + LASTFM_USER + '&api_key=' + LASTFM_API + '&format=json&limit=10';
+if(process.argv[2] == '-f') { // We're using a config file
+    fs.exists("hipfm.json", function(exists) {
+        if (exists) {
+            console.log('Using hipfm config file.');
+            config = (JSON.parse(fs.readFileSync("hipfm.json", "utf8")));
+            fetchTracks(true, config);
+        } else {
+            console.log('The hipfm.json file does not exist. Please create the file before using the -f flag.');
+            process.exit(1);
+        }
+    });
+} else {  // We're using the command line args
+    if (process.argv.length < 6) {
+        console.log('Usage: node server.js ' + ' [Last.fm Api Key] [Last.fm User] [HipChat Admin/Notification Key] [Room] [Display Name]');
+        process.exit(1);
+    }
 
-    http.get(lastfmURL, function(httpRes) {
-        var data = '';
+    config = {
+        "hipchat": {
+            "key": process.argv[4],
+            "room": process.argv[5]
+        },
+        "lastfm": {
+            "users": [
+                {
+                    "displayName": process.argv[6] || 'HipFM',
+                    "username": process.argv[3],
+                    "key": process.argv[2]
+                }
+            ]
+        }
+    }
+    fetchTracks(true, config);
+}
 
-        httpRes.on('data', function (chunk){
-            data += chunk;
+function checkTracks(config) {
+
+    config.lastfm.users.forEach(function(element, index, array){
+        var lastfmURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + config.lastfm.users[index].username + '&api_key=' + config.lastfm.users[index].key + '&format=json&limit=10';
+        http.get(lastfmURL, function(httpRes) {
+            var data = '';
+
+            httpRes.on('data', function (chunk){
+                data += chunk;
+            });
+
+            httpRes.on('end',function(){
+                processData(JSON.parse(data), index);
+            });
+
+
+        }).on('error', function(e) {
+            util.log("ERROR::checkTracks " + e.message);
         });
-
-        httpRes.on('end',function(){
-            processData(JSON.parse(data));
-        });
-
-
-    }).on('error', function(e) {
-        util.log("ERROR::checkTracks " + e.message);
     });
 }
 
-function processData(data) {
+function processData(data, userIndex) {
     if (data.recenttracks && data.recenttracks.track) {
         var currentTrack = data.recenttracks.track[0];
-
-        if (lastTrack != currentTrack.name) {
+        if (config.lastfm.users[userIndex].lastTrack != currentTrack.name) {
             var html = '';
 
             if (currentTrack.image[1]['#text'] !== '') {
@@ -51,19 +77,20 @@ function processData(data) {
             html += ' - <a href="http://www.last.fm/music/'  + currentTrack.artist['#text'] + '">' + currentTrack.artist['#text'] + '</a>';
             html += ', <a href="http://www.last.fm/music/'  + currentTrack.artist['#text'] + '/'+ currentTrack.album['#text'] + '">' + currentTrack.album['#text'] + '</a>';
 
-            sendToHipChat(html);
-            lastTrack = currentTrack.name;
-            util.log(lastTrack + ' - ' + currentTrack.artist['#text'] + ' - ' + currentTrack.album['#text']);
+            sendToHipChat(html, userIndex);
+            config.lastfm.users[userIndex].lastTrack = currentTrack.name;
+            util.log(config.lastfm.users[userIndex].lastTrack + ' - ' + currentTrack.artist['#text'] + ' - ' + currentTrack.album['#text']);
         }    
     }
     
-
-    fetchTracks();
+    if(userIndex==0) { // Only call the timeout callback once to prevent n*2 callbacks each time.
+        fetchTracks(false, config);
+    }
 }
 
-function sendToHipChat(message) {
+function sendToHipChat(message, userIndex) {
     message = encodeURIComponent(message);
-    var url = 'https://api.hipchat.com/v1/rooms/message?format=json&auth_token=' + HIPCHAT_AUTH_TOKEN + '&room_id=' + HIPCHAT_ROOM + '&from=' + HIPCHAT_DISPLAY_NAME + '&color=purple&message_format=html&message=' + message;
+    var url = 'https://api.hipchat.com/v1/rooms/message?format=json&auth_token=' + config.hipchat.key + '&room_id=' + config.hipchat.room + '&from=' + config.lastfm.users[userIndex].displayName + '&color=purple&message_format=html&message=' + message;
 
     https.get(url, function(httpRes) {
         var data = '';
@@ -81,16 +108,13 @@ function sendToHipChat(message) {
 }
 
 
-function fetchTracks(instant) {
+function fetchTracks(instant, config) {
     var millis = instant ? 0 : 30000;
 
     setTimeout(function() {
-        checkTracks();
+        checkTracks(config);
     }, millis);
 }
-
-console.log('Started HipFM service');
-fetchTracks(true);
 
 
 
